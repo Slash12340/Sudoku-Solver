455fe627b85c7b7f5b624a94f01c9e1989cf5441
Akashdeep Singh <sakashdeep40@yahoo.com>
2020-07-12 23:54:56 +0100

    Reformated the file again. Make sure you pull this

--- SodukuSolver.py
+++ SodukuSolver.py
@@ -1,11 +1,13 @@
 import numpy as np
 
+
 class Board:
     def __init__(self):
         self.board = np.zeros(81).reshape(9, 9)
 
     def printBoard(self):
         print(self.board)
+
 
 test = Board()
 test.printBoard()
